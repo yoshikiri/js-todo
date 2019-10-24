6667697b0cf744259301f99c9af12e72251ceaf8
yoshikiri <elise.nicopy@gmail.com>
2019-10-25 00:27:47 +0900

    Change directory.

---
+++ src/EventEmitter.js
@@ -0,0 +1,37 @@
+export class EventEmitter {
+  constructor() {
+    this._listeners = new Map();
+  }
+
+  addEventListener(type, listener) {
+    if (!this._listeners.has(type)) {
+      this._listeners.set(type, new Set());
+    }
+    const listenerSet = this._listeners.get(type);
+    listenerSet.add(listener);
+  }
+
+  emit(type) {
+    const listenerSet = this._listeners.get(type);
+    if (!listenerSet) {
+      return;
+    }
+    listenerSet.forEach(listener => {
+      listener.call(this);
+    });
+  }
+
+  removeEventListener(type, listener) {
+    const listenerSet = this._listeners.get(type);
+    if (!listenerSet) {
+      return;
+    }
+
+    listenerSet
+      .forEach(ownListener => {
+        if (ownListener === listener) {
+          listenerSet.delete(listener);
+        }
+      });
+  }
+}
